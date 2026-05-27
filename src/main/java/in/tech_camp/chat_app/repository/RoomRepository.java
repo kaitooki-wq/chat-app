146844ac40ffa205da95a80c2c9cb1443b53c784
kaitooki-wq <kaito.oki@infomart.co.jp>
2026-05-27 14:28:58 +0900

    チャット画面の編集

--- src/main/java/in/tech_camp/chat_app/repository/RoomRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/RoomRepository.java
@@ -3,6 +3,7 @@ package in.tech_camp.chat_app.repository;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
 
 import in.tech_camp.chat_app.entity.RoomEntity;
 @Mapper
@@ -13,6 +14,8 @@ public interface  RoomRepository {
   void insert(RoomEntity roomEntity); 
   // チャットルームが入力され、DBに保存されたときに、自動的に生成されたキー（ID）が、
   // その場で手元にあるエンティティ（RoomEntity）のidフィールドに自動的に格納されるようになる設定」
-  
   // DBにroomがインサートされる→DBがその部屋のキーを生成してidを決める→それをjava(mybatis)に返して、今挿入された部屋のidは○〇であるとJavaが認識できるようにする　
+  
+  @Select("SELECT * FROM rooms WHERE id = #{id}")
+  RoomEntity findById(Integer id);
 }
--- src/main/java/in/tech_camp/chat_app/repository/RoomUserRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/RoomUserRepository.java
@@ -1,8 +1,13 @@
 package in.tech_camp.chat_app.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.One;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Select;
 
 import in.tech_camp.chat_app.entity.RoomUserEntity;
 
@@ -12,4 +17,31 @@ public interface RoomUserRepository {
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void insert(RoomUserEntity userRoomEntity);
   
+  @Select("SELECT * FROM room_users WHERE user_id = #{userId}")
+  @Result(property = "room", column = "room_id",
+          one = @One(select = "in.tech_camp.chat_app.repository.RoomRepository.findById"))
+  List<RoomUserEntity> findByUserId(Integer userId);
+   
+  /* * 👆【解説：このメソッド(@Select~List)がやっていることと役割】
+   * * 1. 何のためにあるの？
+   * トップページ（ルートパス "/"）を開いた際に、サイドバーへ「ログイン中のユーザーが
+   * 参加しているチャットルームの一覧」を正しく出し分けるために存在します。
+   * * 2. 「自分が作った部屋」と「参加している部屋」は両方出る？
+   * 両方とも漏れなくすべて表示されます！
+   * 部屋を作成した際、作成者（自分）のIDも招待されたメンバーのIDと一緒に 
+   * `room_users` テーブル（中間テーブル）に保存される仕組みになっているため、
+   * 自分が作ったか他人が作ったかに関係なく、自分が所属する部屋がすべてヒットします。
+   * * 3. 処理の具体的な流れ（MyBatisの連携技）
+   * ① @Select
+   * 中間テーブル（room_users）から、現在ログインしているユーザーのID（#{userId}）が
+   * 含まれるレコードをすべて特定して抜き出します。
+   * ② @Result と @One
+   * ①で抜き出したレコードの `room_id` を使って、自動的に `RoomRepository.findById` を
+   * 裏で見にいき、「チャットルームの名前（name）」などの詳細情報をガッチャンコ（結合）します。
+   * * 4. 注意点（画面遷移について）
+   * このコード単体では「チャットルームを押したらその部屋に飛ぶ」という画面遷移の動きにはなりません。
+   * これはあくまで「サイドバーに自分の部屋一覧を表示するためのデータをDBから持ってくる」処理です。
+   * クリックして中に進む動きは、この後の章でHTMLにリンク（href）を設定し、コントローラーに
+   * 部屋ごとのメッセージ画面を返す処理を追加することで完成します。
+   */
 }
