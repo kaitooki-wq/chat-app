6d83fc4e9839614d6892e9e5a6419d820a44afde
kaitooki-wq <kaito.oki@infomart.co.jp>
2026-05-28 09:37:06 +0900

    メッセージの表示

--- src/main/java/in/tech_camp/chat_app/controller/MessageController.java
+++ src/main/java/in/tech_camp/chat_app/controller/MessageController.java
@@ -49,6 +49,9 @@ public class MessageController {
         model.addAttribute("messageForm", new MessageForm());
         model.addAttribute("roomId", roomId);
 
+        List<MessageEntity> messages = messageRepository.findByRoomId(roomId);
+        model.addAttribute("messages", messages);
+
     return "messages/index";
   }
 
