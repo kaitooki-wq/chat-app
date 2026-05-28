58e9b7f55eed6f90befda931d7b44cbc8071e555
kaitooki-wq <kaito.oki@infomart.co.jp>
2026-05-28 13:17:10 +0900

    画像表示の実装

--- src/main/java/in/tech_camp/chat_app/form/MessageForm.java
+++ src/main/java/in/tech_camp/chat_app/form/MessageForm.java
@@ -2,6 +2,7 @@ package in.tech_camp.chat_app.form;
 
 import in.tech_camp.chat_app.validation.ValidationPriority1;
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.web.multipart.MultipartFile;
 import lombok.Data;
 
 @Data
@@ -9,4 +10,5 @@ public class MessageForm {
   @NotBlank(message = "message can't be blank",groups = ValidationPriority1.class)
   private String content;
   
+  private MultipartFile image;
 }
